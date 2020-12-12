4ea94b3dbfac920220882f51a05541cdf5ab9f6f
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2020-12-12 22:51:25 +0400

    The mall of Youth updated

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function openNav() {
   document.getElementById("mySide-menu").style.width = "250px";
   document.getElementById("main").style.marginLeft = "150px";
+  document.getElementById("myMain-content").style.backgroundColor =
+    "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySide-menu").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("myMain-content").style.backgroundColor = "white";
 }
