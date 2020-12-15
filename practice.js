668461f60313abcb7c963ba6ff805708f06b4c3c
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2020-12-15 07:38:22 +0400

    Updates

--- practice.js
+++ practice.js
@@ -2,5 +2,125 @@
 // alert("hello people");
 // console.log("hello world");
 
-let name = 3;
-console.log(name);
+// let name = "Siyuan1";
+// let address, zip, state;
+// address = "Siyuan";
+// zip = "1234566";
+// console.log(address, zip);
+
+// const name = "Siyuan";
+// console.log(name);
+
+// const name = "Siyuan";
+// const lastName = "Ruan";
+// let fullName = name + " " + lastName;
+
+// console.log("Hello my Name is " + fullName);
+
+// const webiste = "google";
+// const url = "https://www." + webiste + ".com";
+// console.log(url);
+
+// const number = 34;
+// const number1 = 2.4587;
+// const add = number + number1;
+// const sub = number - number1;
+// const mult = number * number1;
+// const div = number / number1;
+
+// console.log(add);
+// console.log(sub);
+// console.log(mult);
+// console.log(div);
+// console.log(add);
+
+// let number = 40;
+// console.log(number);
+
+// const slices = 10;
+// const children = 4;
+// const amount = slices % children;
+
+// const random = (4 + 6 + 7) * 10;
+// console.log(random);
+// console.log(amount);
+
+// const value = name - lastName;
+// console.log(value);
+
+// let number = "10";
+// let number1 = "15";
+// const result = number - number1;
+
+// console.log(result);
+
+// const number = 45;
+// console.log(typeof number);
+
+// const friends = ["Ananya", "Jeanette", "Dhanishtha", 45, undefined, null];
+
+// console.log(friends[2]);
+// friends[4] = "Hello World";
+// console.log(friends[4]);
+// let bestFriend = friends[1];
+// console.log(bestFriend);
+
+// function greet(name) {
+//   console.log("Hello There " + name);
+// }
+
+// greet("Bob");
+// greet("anna");
+
+// const wallHeight = "80";
+
+// function calculate(area) {
+//   const newValue = area * 7;
+//   return newValue;
+// }
+
+// const width = calculate(100);
+// const height = calculate(wallHeight);
+// const dimensions = [width, height];
+
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
+
+// const thirdValue = add(5, 6);
+// console.log(thirdValue);
+
+// const person = {
+//   name: "SIYUAN",
+//   lastName: "Ruan",
+//   age: 17,
+//   education: true,
+//   sibling: ["h", "y", "x"],
+//   greeting: function () {
+//     console.log("Hello World");
+//   },
+// };
+
+// const age = person.age;
+// console.log(age);
+// person.name = "Siyuuan";
+// console.log(person.name);
+// console.log(person.sibling[2]);
+// person.greeting();
+
+// const value = true;
+
+// if (value) {
+//   console.log("Hello World");
+// } else {
+//   console.log("Hello Hello");
+// }
+
+const num1 = 4;
+const num2 = 6;
+
+if (num1 > num2) {
+  console.log("first number is bigger than second");
+} else {
+  console.log("second number is bigger than the first");
+}
