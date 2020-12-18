6eb5deb49aed69b03406fdf8a94666e51fb8e3b3
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2020-12-18 13:04:01 +0400

    New

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function closeNav() {
   document.getElementById("myMain-content").style.backgroundColor = "white";
 }
 
-var btnContainer = document.getElementById("myShop");
-var btns = btnContainer.getElementsByClassName("btns");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("btns");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// var btnContainer = document.getElementById("myShop");
+// var btns = btnContainer.getElementsByClassName("btns");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("btns");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
