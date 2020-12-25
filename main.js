4dd3c35e808b927d4a5f16585e856e8bc5d846f0
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2020-12-26 00:13:21 +0400

    Homepage Updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function openNav() {
   document.getElementById("mySide-menu").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "75px";
+  document.getElementById("main").style.marginLeft = "85px";
   document.getElementById("myMain-content").style.backgroundColor =
     "rgba(0,0,0,0.4)";
 }
