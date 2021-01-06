df17ed8b8d2b9a68edb25519571cdb2c4f861bd8
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2021-01-06 23:59:53 +0400

    Update to htdocs

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,15 +6,29 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="index.php" method="get">
-      <input type="text" name="name" />
-      <input type="number" name="age">
+  <!-- <form action="index.php" method="post">
+      <input type="number" name="num1" />
+      <br>
+      <br>
+      <input type="number" name="num2">
       <input type="submit" />
-    </form>
+    </form> -->
+    <!-- <h1>Hello</h1> -->
+<?php include "login.html" ?>
+
+
 </body>
 </html>
 
 <?php
+
+// echo "Answer:", $_POST["num1"]+ $_POST["num2"];
+
+
+
+
+
+
   // $name = "John";
   // $age = 35;
   // echo"<h1>Hello World $name</h1>";
@@ -43,7 +57,100 @@
   // echo round(3.2234242432);
   // echo ceil(3.5);
   // echo floor(5.9);
-  echo "Your name is:", $_GET["name"];
-  echo "Your age is:", $_GET["age"];
+  // echo "Your name is:", $_GET["name"];
+  // echo "Your age is:", $_GET["age"];
+
+
+  // define('GREETING',"Hello World",true);
+  // echo greeting
+  // $people = array('Kevin', 'Jeremy', 'Sara');
+  // echo $people[1];
+  // $ids = array(23,55,12);
+  // $cars = ['Honda', 'Toyota', 'Ford'];
+  // $cars[3] = 'Chevy';
+  // $cars[] = 'BMW';
+  // echo $cars[4];
+  // echo count($cars)
+  // print_r($cars);
+  // var_dump($cars);
+  // $people = array('Brad' => 35, 'Jose' =>12, "Will" => 45);
+  // echo $people['Brad'];
+  // print_r($people);
+
+
+  // $ids = [22 => 'Brad', 44 =>'Jose', 55 => 'Will'];
+  // echo $ids[22]
+  // $people['Jill'] = 42;
+  // echo $people['Jill'];
+//   $cars = array(
+//     array('Honda',20,10), 
+//   array('Toyota', 30, 20), 
+//   array('Yuils', 20, 11)
+  
+// );
+
+// echo $cars[1][0];
+
+// for($i = 5; $i<10; $i++){
+//   echo "Hello", $i;
+//   echo "<br>";
+// }
+
+// $i = 0;
+// while($i <10){
+//   echo $i;
+//   echo "<h1>Hello World</h1>";
+//   $i++;
+// }
+
+// $i = 0;
+
+// do{
+//   echo "Hello World";
+//   echo "<br>";
+//   $i++;
+// }
+// while($i<10);
+
+// $people = ['Brad' => 'Brad@gmail.com', 'Jose' => 'Jose@gmail.com', 'Will'=> 'Will@gmail.com'];
+// foreach($people as $person => $email){
+//   echo $person, " ", 'and' , " ", $email;
+//   echo "<br>";
+// }
+
+// function main(){
+// echo "Hello World";
+
+// }
+
+// main();
+// function Hello($name = 'Siyuan'){
+//   echo "Hello $name<br>";
+// }
+
+// Hello();
+
+// function addNumbers($num1,$num2){
+//   return $num1 + $num2;
+// }
+// echo addNumbers(5,6)
+
+// $myNum = 10;
+// function addFive($num){
+//    $num += 5;
+ 
+// }
+// function addTen(&$num){
+//    $num += 10;
+// }
+// addFive($myNum);
+// echo "Value :$myNum<br>";
+// addTen($myNum);
+// echo "Value: $myNum<br>";
+
+// $num = 5;
+// if($num === 5){
+//   echo "5 passed";
+// }
 
 ?>
