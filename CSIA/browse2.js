8ea72e7d8ad3b274be4a9342f9b09238fbec5208
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2021-10-05 16:03:51 +0400

    Occupation

--- CSIA/browse2.js
+++ CSIA/browse2.js
@@ -1,4 +1,31 @@
 usersList = document.querySelector(".users .users-list");
-const searchBar = document.querySelector(".search1 input");
-searchBtn = document.querySelector(".search1 button");
+const searchBar1 = document.querySelector(".search1 input");
+searchBtn1 = document.querySelector(".search1 button");
 
+searchBtn1.onclick = ()=>{
+    console.log("Hello");
+}
+
+searchBar1.onkeyup = () =>{
+    let searchTerm = searchBar1.value;
+    console.log(searchTerm);
+    if(searchTerm != ""){
+        searchBar.classList.add("active");
+    }
+    else{
+        searchBar.classList.remove("active");
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "browseOccu.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                usersList.innerHTML = data;
+                console.log(data);
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+}
--- CSIA/browseOccu.php
+++ CSIA/browseOccu.php
@@ -5,7 +5,7 @@ $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
 
 $output = "";
 $outgoing_id = $_SESSION['unique_id'];
-$sql = mysqli_query($conn, "SELECT * FROM userinfo WHERE NOT uniqueid = '{$outgoing_id}' AND gender='{$searchTerm}' AND participant = 'listener'");
+$sql = mysqli_query($conn, "SELECT * FROM userinfo WHERE NOT uniqueid = '{$outgoing_id}' AND worker='{$searchTerm}' AND participant = 'listener'");
 
 
 if(mysqli_num_rows($sql) > 0){
@@ -20,7 +20,7 @@ if(mysqli_num_rows($sql) > 0){
     }
 }
 else{
-    $output .= "please type 'male' or 'female'";
+    $output .= "please type 'student' or 'teacher', If there is no result, that means there is no student or teacher";
 }
 
 echo $output;
